ea758d957121649b0e10414bd80363cf105798bc
aboynejames <james@aboynejames.co.uk>
2020-09-20 19:51:00 +0100

    update packages

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+'use strict'
 import RcComposer from './rcComposer.js'
 import KbidComposer from './kbidComposer.js'
 import RcUtility from './rcUtility.js'
 import CryptoUtility from './cryptoUtility.js'
+/**
+*  Interface to reference contract libraries
+*
+*
+* @class LibraryLib
+* @package    Network Library
+* @copyright  Copyright (c) 2020 James Littlejohn
+* @license    http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+* @version    $Id$
+*/
+import util from 'util'
+import events from 'events'
 
-const liveComposer = new RcComposer()
-const liveKBID = new KbidComposer()
-const liveRcUtility = new RcUtility()
-const liveCryptoUtilty = new CryptoUtility()
+var LibraryLib = function () {
+  events.EventEmitter.call(this)
+  console.log('ref contract composer live')
+  this.liveComposer = new RcComposer()
+  this.liveKBID = new KbidComposer()
+  this.liveLibraryLib = new RcUtility()
+  this.liveCryptoUtilty = new CryptoUtility()
 
-export default liveRcUtility
+}
+
+/**
+* inherits core emitter class within this class
+* @method inherits
+*/
+util.inherits(LibraryLib, events.EventEmitter)
+
+export default LibraryLib
